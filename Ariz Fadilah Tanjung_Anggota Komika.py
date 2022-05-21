7af756d28b93c3417b8065013cd45473b0f6547d
Ariz29 <92160976+Ariz29@users.noreply.github.com>
2022-05-21 19:14:35 +0700

    Add files via upload

---
+++ Ariz Fadilah Tanjung_Anggota Komika.py
@@ -0,0 +1,68 @@
+## Latihan 1
+print("Biodata Idolaku")
+nama = "Prof. Dr.ing. Ir. H. Bacharuddin Jusuf Habibie"
+umur = "85 tahun"
+ttl  = "25 Juni 1936, Gorontalo, Sulawesi"
+
+print("Nama : ",nama)
+print("umur :",umur)
+print("tempat dan tanggal lahir",ttl)
+
+##Latihan 2
+nama_user = "Ariz Fadilah Tanjung"
+print('Halo,',nama_user)
+
+#Latihan 3
+panjang = int(input("Masukkan Nilai = "))
+lebar = int(input("Masukkan Nilai = "))
+luas_ruangan = panjang*lebar
+print(luas_ruangan,"meter")
+
+#Latihan 4
+n = int(input("Masukkan Nilai Anda = "))
+##Rumus Input data
+rumus = (n)*(n+1)/2
+##Hasil Rumus
+print(int(rumus))
+
+#Latihan 5
+inci = int(input("Masukkan Nilainya (inci) = "))
+ft = int(input("Masukkan Nilainya (kaki)= "))
+#rumus in ke cm dan ft ke cm
+rumus1 = inci/0.39370
+rumus3 = ft/0.032808
+#hasilnya 
+print(rumus1)
+print(rumus3)
+
+#Latihan 6
+print("===Konversi Data===")
+
+kaki = int(input("Masukkan Datanya ="))
+
+rumus_inci = kaki*12
+rumus_yard = kaki*0.33333
+rumus_mil = kaki/5.280
+
+print(rumus_inci)
+print(rumus_yard)
+print(rumus_mil)
+
+#Latihan 7
+panjang_alas = int(input("Masukkan Panjang = "))
+tinggi = int(input("Masukkan Tinggi = "))
+
+area = panjang_alas*tinggi/2
+
+print(int(area))
+
+#Latihan 8
+weight = int(input("Masukkan Nilainya = "))
+height = int(input("Masukkan Nilainya = "))
+
+BMI = weight/(height*height)
+
+print(BMI)
+
+#Latihan 9
+
